2b19048bd2fcc8a6841b34853bd898afac519cb5
callium <conner610@gmail.com>
2018-01-12 13:09:11 -0500

    Better idea about shooting/movement

--- isaac_input/window_controls.py
+++ isaac_input/window_controls.py
@@ -1,6 +1,13 @@
 import os
 from AppKit import NSWorkspace
 
+# This will open the game
+def open_isaac():
+    command = """
+        open steam://run/250900
+    """
+    os.system(command)
+
 # Uses applescript to move the Binding of Isaac window to the foreground
 # Later, I will try to create a python module to do this using system codes
 # rather than applescript
