74a6413ce1f75b2328d4c07a0b0465fbe6831368
callium <conner610@gmail.com>
2018-03-02 14:16:50 -0500

    Changes to test.py

--- isaac/test.py
+++ isaac/test.py
@@ -1,9 +1,9 @@
 # Here is a testing playground
 import genetics as gen
 
-def main():
-
-    # This will be done every x seconds
+# Try and create a frame(will be done every x seconds), then
+# add that frame to the current gene(or run in the game)
+def test_frames_and_genes():
     test_frame = gen.Frame()
     test_frame.populate((2, 5), [(1, 2),(3, 4)], 2, 5)
     test_frame.print_frame()
@@ -13,5 +13,11 @@ def main():
     test_gene.add_frame(test_frame)
     test_gene.print_gene()
 
+
+def main():
+    test_frames_and_genes()
+    
+
 if __name__ == "__main__":
     main()
+
