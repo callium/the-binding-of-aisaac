0c5a0431c61706b48ac51ee80c4ab6e56cd88959
callium <conner610@gmail.com>
2018-04-18 23:36:29 -0400

    I think the tf feature columns are working

--- isaac/isaac_estimator.py
+++ isaac/isaac_estimator.py
@@ -1,30 +1,82 @@
 import tensorflow as tf
 import numpy as np
 
-feature_columns = [
-        tf.feature_column.numeric_column("IsEnemyAbove"),
-        tf.feature_column.numeric_column("IsEnemyBelow"),
-        tf.feature_column.numeric_column("IsEnemyLeft"),
-        tf.feature_column.numeric_column("IsEnemyRight")
-    ]
-
-_CSV_COLUMNS = ['IsEnemyAbove', 'IsEnemyBelow', 'IsEnemyLeft', 'IsEnemyRight', 'MovementDir', 'ShotDir']
+_CSV_COLUMNS = ['is_enemy_above', 'is_enemy_below', 'is_enemy_left', 'is_enemy_right', 'movement_dir', 'shot_dir']
 _CSV_COLUMN_DEFAULTS = [[''],[''],[''],[''],[''],['']]
 
-def input_fn():
-    # Get all of the training data
-    lines = [line.rstrip('\n') for line in open('training_data.csv')]
-    # OR
-    dataset = tf.data.TextLineDataset("training_data.csv")
+def build_model_columns():
+    # is_enemy_above = tf.feature_column.categorical_column_with_vocabulary_list(
+    # 'is_enemy_above', [
+    #     '0', '1'])
+        
+    # is_enemy_below = tf.feature_column.categorical_column_with_vocabulary_list(
+    # 'is_enemy_below', [
+    #     '0', '1'])
 
-    def parse_data(value):
+    # is_enemy_left = tf.feature_column.categorical_column_with_vocabulary_list(
+    # 'is_enemy_left', [
+    #     '0', '1'])
+
+    # is_enemy_right = tf.feature_column.categorical_column_with_vocabulary_list(
+    # 'is_enemy_right', [
+    #     '0', '1'])
+
+    # movement_dir = tf.feature_column.categorical_column_with_vocabulary_list(
+    # 'movement_dir', [
+    #     '0', '1', '2', '3', '4'])
+
+    # shot_dir = tf.feature_column.categorical_column_with_vocabulary_list(
+    # 'shot_dir', [
+    #     '0', '1', '2', '3', '4'])
+
+    is_enemy_above = tf.feature_column.numeric_column('is_enemy_above')
+    is_enemy_below = tf.feature_column.numeric_column('is_enemy_below')
+    is_enemy_left = tf.feature_column.numeric_column('is_enemy_left')
+    is_enemy_right = tf.feature_column.numeric_column('is_enemy_right')
+    movement_dir = tf.feature_column.numeric_column('movement_dir')
+    shot_dir = tf.feature_column.numeric_column('shot_dir')
+
+    # Wide columns and deep columns.
+    feature_columns = [
+        is_enemy_above, is_enemy_below, is_enemy_left, is_enemy_right, movement_dir,
+        shot_dir,
+    ]
+
+    # return base_columns + crossed_columns
+    return feature_columns
+
+def input_fn():
+    dataset = tf.data.TextLineDataset("training_data.csv")
+    def parse_csv(value):
         columns = tf.decode_csv(value, record_defaults=_CSV_COLUMN_DEFAULTS)
         features = dict(zip(_CSV_COLUMNS, columns))
-        labels = features.pop('MovementDir')
+        labels = features.pop('movement_dir')
         return features, labels
 
-    dataset = dataset.map(parse_data, num_parallel_calls=5)
+    dataset = dataset.map(parse_csv, num_parallel_calls=5)
     dataset = dataset.repeat(40)
     dataset = dataset.batch(40)
-
     return dataset
+
+
+def build_estimator():
+    feature_columns = build_model_columns()
+    model = tf.estimator.DNNClassifier(
+        feature_columns=feature_columns,
+        hidden_units=[10, 10],
+        n_classes=4)
+    return model
+
+def main():
+    classifier = build_estimator()
+
+    # classifier.train(
+    #     input_fn=lambda:input_fn(),steps=50)
+
+    # eval_result = classifier.evaluate(
+    #     input_fn=lambda:input_fn())
+
+    # print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
+
+if __name__ == "__main__":
+    main()
